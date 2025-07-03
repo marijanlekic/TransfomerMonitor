94be86a4246e06e52200377aa1ed907c9c0df82f
marijanlekic <marijanlekic89@gmail.com>
2025-07-03 13:26:29 +0200

    initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,45 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection
+} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {HttpClient, provideHttpClient, withInterceptors} from '@angular/common/http';
+import {provideAnimations} from '@angular/platform-browser/animations';
 
-import { routes } from './app.routes';
+import * as PlotlyJS from 'plotly.js-basic-dist';
+import {ToastrModule} from 'ngx-toastr';
+import {PlotlyModule} from 'angular-plotly.js';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+
+import {errorInterceptor} from '@core/interceptors/error.interceptor';
+import {ThemeService} from '@core/services/theme.service';
+import {LanguageService} from '@core/services/language.service';
+
+import {routes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
+
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    provideHttpClient( withInterceptors([errorInterceptor]),),
+    provideAnimations(),
+    ThemeService,
+    LanguageService,
+    importProvidersFrom(
+      PlotlyModule.forRoot(PlotlyJS),
+      ToastrModule.forRoot({positionClass: 'toast-bottom-center', disableTimeOut: true, closeButton: true}),
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: (http: HttpClient) => new TranslateHttpLoader(http, './assets/i18n/', '.json'),
+          deps: [HttpClient]
+        }
+      })
+    )
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+import {NotFoundPageComponent} from './features/not-found-page/not-found-page.component';
+
+export const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    data: {title: 'PAGES.HOME.PAGE_TITLE', animation: 'HomePage'},
+    loadComponent: () =>
+      import('./features/home-page/home-page.component')
+        .then(m => m.HomePageComponent)
+  },
+  {
+    path: 'monitoring',
+    data: {title: 'PAGES.MONITORING.PAGE_TITLE', animation: 'MonitoringPage'},
+    loadComponent: () =>
+      import('./features/monitoring-page/monitoring-page.component')
+        .then(m => m.MonitoringPageComponent)
+  },
+  {
+    path: '404',
+    data: {animation: 'NotFoundPage'},
+    component: NotFoundPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+];
